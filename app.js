08fe61136b684702c67bf5fb23ff2374082bdfa4
velmuruganfinance <velmuruganfinance123@gmail.com>
2026-08-15 14:52:38 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -302,18 +302,40 @@ class FinanceApp {
           });
 
           if (fetchedGroups.length > 0) {
-            const sanitizedLocalObj = JSON.parse(JSON.stringify(this.state.groups));
-            if (!this.deepEqual(sanitizedLocalObj, fetchedGroups)) {
-              this.state.groups = fetchedGroups;
-              this.sortAllMembers();
-              this.migrateLegacyData();
-              try {
-                localStorage.setItem("vm_finance_groups", JSON.stringify(this.state.groups));
-                this.updateStorageStatusBadge();
-              } catch (e) {
-                console.error(e);
+            let cloudIsOutdated = false;
+            const fetchedMap = new Map(fetchedGroups.map(g => [g.id, g]));
+            
+            for (const localGroup of this.state.groups) {
+              const fetchedGroup = fetchedMap.get(localGroup.id);
+              if (fetchedGroup) {
+                const localTime = localGroup.lastModified || 0;
+                const fetchedTime = fetchedGroup.lastModified || 0;
+                if (localTime > fetchedTime) {
+                  cloudIsOutdated = true;
+                  break;
+                }
+              } else {
+                 cloudIsOutdated = true;
+                 break;
               }
-              this.render();
+            }
+
+            if (cloudIsOutdated) {
+              this.uploadAllGroupsToFirestore();
+            } else {
+              const sanitizedLocalObj = JSON.parse(JSON.stringify(this.state.groups));
+              if (!this.deepEqual(sanitizedLocalObj, fetchedGroups)) {
+                this.state.groups = fetchedGroups;
+                this.sortAllMembers();
+                this.migrateLegacyData();
+                try {
+                  localStorage.setItem("vm_finance_groups", JSON.stringify(this.state.groups));
+                  this.updateStorageStatusBadge();
+                } catch (e) {
+                  console.error(e);
+                }
+                this.render();
+              }
             }
             this.updateCloudStatus("synced", "Cloud Synced ✓");
           } else {
